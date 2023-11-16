318135e8ef4f6d753c7712c41e552d469b21927b
Kanan Mirzayev <58333179+KananMirza@users.noreply.github.com>
2023-11-16 08:27:10 +0400

    Added JWT security

---
+++ src/main/java/com/orient/library/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.orient.library.controller;
+
+import com.orient.library.auth.TokenManager;
+import com.orient.library.dto.request.LoginRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final TokenManager tokenManager;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),loginRequest.getPassword())
+        );
+        return ResponseEntity.ok(tokenManager.generateToken(loginRequest.getEmail()));
+    }
+}
