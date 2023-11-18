009447223a89e6ba2c0d6843133241d59531c166
Kanan Mirzayev <58333179+KananMirza@users.noreply.github.com>
2023-11-19 00:04:09 +0400

    ResponseApi

--- src/main/java/com/orient/library/controller/AuthController.java
+++ src/main/java/com/orient/library/controller/AuthController.java
@@ -2,7 +2,10 @@ package com.orient.library.controller;
 
 import com.orient.library.auth.TokenManager;
 import com.orient.library.dto.request.LoginRequest;
+import com.orient.library.enums.Message;
+import com.orient.library.response.ResponseApi;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,12 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final TokenManager tokenManager;
     private final AuthenticationManager authenticationManager;
+    private final ResponseApi responseApi;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<ResponseApi> login(@RequestBody LoginRequest loginRequest){
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),loginRequest.getPassword())
         );
-        return ResponseEntity.ok(tokenManager.generateToken(loginRequest.getEmail()));
+        return ResponseEntity.ok(responseApi(HttpStatus.OK.value(), Message.SUCCESS.value(), tokenManager.generateToken(loginRequest.getEmail())));
+    }
+
+    private ResponseApi responseApi(Integer status,String message,Object object){
+        responseApi.setMessage(message);
+        responseApi.setStatus(status);
+        responseApi.setBody(object);
+        return responseApi;
     }
 }
